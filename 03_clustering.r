5ecd170d54659f8ab2886784d61fe8f80216415f
Brian Spiering <bspiering@gmail.com>
2015-03-04 19:49:16 -0800

    Simple clustering

--- 03_clustering.r
+++ 03_clustering.r
@@ -6,3 +6,22 @@ rm(list=ls()) # Delete all variables
 cat("\014")   # Clear console
 
 # Find clusters ----------------------------------------------------------------
+
+# Make copy of iris
+iris_no_labels <- iris
+
+# Remove category labels
+iris_no_labels$Species <- NULL
+
+# Apply kmeans to iris
+n_clusters <- 3
+kc <- kmeans(iris_no_labels, n_clusters)
+# print(kc)
+
+# Visualize the clusters
+plot(iris[c("Sepal.Length", "Sepal.Width")], 
+     col=kc$cluster)
+points(kc$centers[,c("Sepal.Length", "Sepal.Width")], 
+       col=1:3, 
+       pch=8,
+       cex=2)

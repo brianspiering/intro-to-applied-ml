bd2e629f46de1acea1415a20076c43fc2292495b
Brian Spiering <bspiering@gmail.com>
2015-03-04 18:59:04 -0800

    Place holder files

---
+++ 01_linear_regression.r
@@ -0,0 +1,8 @@
+# File description -------------------------------------------------------------
+# Simple example of linear regression
+
+# Setup ------------------------------------------------------------------------
+rm(list=ls()) # Delete all variables
+cat("\014")   # Clear console
+
+# Fit linear regession ---------------------------------------------------------

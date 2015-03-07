48171085fbca52ab0419d309a28f9b74b40f29fb
Brian Spiering <bspiering@gmail.com>
2015-03-07 12:14:58 -0800

    Cleanup

--- 03_clustering.r
+++ 03_clustering.r
@@ -3,10 +3,10 @@
 
 # Setup ------------------------------------------------------------------------
 rm(list=ls()) # Delete all variables
+graphics.off() # Close all open plots
 cat("\014")   # Clear console
 
-# Find clusters ----------------------------------------------------------------
-# Visualize the data
+# Visualize the data -----------------------------------------------------------
 pairs(iris[1:4], 
       main = "Iris Data", 
       pch = 21, 
@@ -14,12 +14,13 @@ pairs(iris[1:4],
       lower.panel=NULL, 
       labels=c("Sepal.Length","Sepal.Width","Petal.Length","Petal.Width"), 
       font.labels=2,
-      cex.labels=2.5) 
+      cex.labels=1.8) 
 
 # Inspect the data
 print(head(iris))
 print(unique(iris$Species))
 
+# Find clusters ----------------------------------------------------------------
 # Make copy of iris
 iris_no_labels <- iris
 
@@ -28,7 +29,8 @@ iris_no_labels$Species <- NULL
 
 # Apply kmeans to iris
 n_clusters <- 3
-kc <- kmeans(iris_no_labels, n_clusters)
+kc <- kmeans(iris_no_labels, 
+             n_clusters)
 # print(kc)
 
 # Visualize the clusters
